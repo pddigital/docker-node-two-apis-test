597ae67b6edb4a4ad29bf229d3cf90c59c63c318
Paul Day (DAL - DALIT - IT) <paul.day.m@gmail.com>
2018-03-27 10:15:08 -0500

    networked two docker containers

--- app1/server.js
+++ app1/server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  fetch('https://api-prod.enro.com/api/nav/main-nav').then(response => {
+  fetch('http://192.168.0.2:8090/api/test').then(response => {
     return response.json()
   }).then(response => {
     return res.status(200).json(response);
