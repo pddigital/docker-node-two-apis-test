12eba085a44e40e00bdfe3b1768d639d5ad1cd17
Paul Day (DAL - DALIT - IT) <paul.day.m@gmail.com>
2018-03-30 12:01:58 -0500

    fixes to docker

--- app2/server.js
+++ app2/server.js
@@ -1,4 +1,17 @@
 const express = require('express');
+const redis = require("redis");
+const fetch = require("isomorphic-unfetch");
+
+const redisClient = redis.createClient({
+  host: "192.168.0.1", // The redis's server ip
+  port: "6379"
+});
+
+redisClient.on("connect", err => {
+  console.log("Connected to Redis!");
+});
+
+console.log('test 2')
 
 // Constants
 const PORT = 8090;
@@ -12,7 +25,20 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/test', (req, res) => {
-  res.status(200).json({content: "This is test content from an API"})
+  redisClient.set('testkey', 'and a test value from redis');
+  fetch("https://swapi.co/api/planets/1/")
+    .then(response => {
+      return response.json();
+    })
+    .then(response => {
+      redisClient.get('testkey', (err, content) => {
+        return res.status(200).json(response.name + ' ' + content);
+      })
+    })
+    .catch(err => {
+
+      console.log(err);
+    });
 });
 
 app.listen(PORT, HOST);
